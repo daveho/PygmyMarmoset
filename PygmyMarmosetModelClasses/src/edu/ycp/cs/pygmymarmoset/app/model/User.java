21d343125cdc05bf5eb0c2a76ef77d3c925732b3
David Hovemeyer <david.hovemeyer@gmail.com>
2017-03-24 17:01:25 -0400

    start model classes project

---
+++ PygmyMarmosetModelClasses/src/edu/ycp/cs/pygmymarmoset/app/model/User.java
@@ -0,0 +1,10 @@
+package edu.ycp.cs.pygmymarmoset.app.model;
+
+public class User {
+	private int id;
+	private String username;
+	private String firstName;
+	private String lastName;
+	private String passwordHash;
+	private boolean superUser;
+}
