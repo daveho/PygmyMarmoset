b2ee91b10550a1efb75493458fa61520801d46d3
David Hovemeyer <david.hovemeyer@gmail.com>
2017-03-28 16:19:36 -0400

    class for login credentials

---
+++ PygmyMarmosetModelClasses/src/edu/ycp/cs/pygmymarmoset/app/model/LoginCredentials.java
@@ -0,0 +1,26 @@
+package edu.ycp.cs.pygmymarmoset.app.model;
+
+public class LoginCredentials {
+	private String username;
+	private String password;
+	
+	public LoginCredentials() {
+		
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
