334b9d9db54a6f3a6f22251699db599f5edce824
David Hovemeyer <david.hovemeyer@gmail.com>
2017-04-10 11:30:36 -0400

    get student projects query to return only visible projects
    I think the original query was actually correct: having
    
      p.visible = 1
    
    in the where clause should have prevented any results
    where p.visible = 0.  Maybe I don't understand left outer
    joins well enough.

--- PygmyMarmosetPersistence/src/edu/ycp/cs/pygmymarmoset/model/persist/txn/GetStudentProjects.java
+++ PygmyMarmosetPersistence/src/edu/ycp/cs/pygmymarmoset/model/persist/txn/GetStudentProjects.java
@@ -29,13 +29,13 @@ public class GetStudentProjects extends DatabaseRunnable<List<Pair<Project, Inte
 		PreparedStatement stmt = prepareStatement(
 				conn,
 				"select p.*, max(s.submissionnumber)" +
-				" from projects as p left join submissions as s" +
+				" from (select * from projects where courseid = ? and visible = 1) as p" +
+				" left join submissions as s" +
 				"   on p.id = s.userid" +
-				" where p.courseid = ?" +
-				"  and s.userid is null or s.userid = ?" +
-				"  and p.visible = 1" +
+				" where s.userid is null or s.userid = ?" +
 				" group by p.id" +
-				" order by p.ontime desc, p.late desc, p.name asc, p.id asc");
+				" order by p.ontime desc, p.late desc, p.name asc, p.id asc"
+				);
 		stmt.setInt(1, course.getId());
 		stmt.setInt(2, user.getId());
 		ResultSet resultSet = executeQuery(stmt);
@@ -43,11 +43,6 @@ public class GetStudentProjects extends DatabaseRunnable<List<Pair<Project, Inte
 		while (resultSet.next()) {
 			Project project = new Project();
 			int index = Query.loadFields(project, resultSet);
-			if (!project.isVisible()) {
-				// Due to the left outer join, invisible projects
-				// can be returned, so don't include them.
-				continue;
-			}
 			Integer maxSub = (Integer) resultSet.getObject(index);
 			if (maxSub == null) {
 				maxSub = 0;
