d9834807248f30e00dd0eb25ec247d3d2cadcbb7
David Hovemeyer <david.hovemeyer@gmail.com>
2017-04-11 10:39:31 -0400

    query to get student projects and count submissions is working now

--- PygmyMarmosetPersistence/src/edu/ycp/cs/pygmymarmoset/model/persist/txn/GetStudentProjects.java
+++ PygmyMarmosetPersistence/src/edu/ycp/cs/pygmymarmoset/model/persist/txn/GetStudentProjects.java
@@ -26,28 +26,33 @@ public class GetStudentProjects extends DatabaseRunnable<List<Pair<Project, Inte
 
 	@Override
 	public List<Pair<Project, Integer>> execute(Connection conn) throws SQLException {
+		// This query is done in a somewhat strange way (left join on
+		// a subquery) because I was experiencing strange results
+		// with a more straightforward approach.
 		PreparedStatement stmt = prepareStatement(
 				conn,
-				"select p.*, max(s.submissionnumber)" +
-				" from (select * from projects where courseid = ? and visible = 1) as p" +
-				" left join submissions as s" +
-				"   on p.id = s.projectid" +
-				" where s.userid is null or s.userid = ?" +
-				" group by p.id" +
+				"select p.*, x.*" +
+				"  from projects as p" +
+				" left join (select s.projectid, count(*)" +
+				"             from submissions as s" +
+				"            where s.userid = ?" +
+				"            group by s.projectid) as x" +
+				"   on p.id = x.projectid" +
+				" where p.courseid = ? and p.visible = 1" +
 				" order by p.ontime desc, p.late desc, p.name asc, p.id asc"
 				);
-		stmt.setInt(1, course.getId());
-		stmt.setInt(2, student.getId());
+		stmt.setInt(1, student.getId());
+		stmt.setInt(2, course.getId());
 		ResultSet resultSet = executeQuery(stmt);
 		List<Pair<Project, Integer>> result = new ArrayList<>();
 		while (resultSet.next()) {
 			Project project = new Project();
 			int index = Query.loadFields(project, resultSet);
-			Integer maxSub = (Integer) resultSet.getObject(index);
+			Number maxSub = (Number) resultSet.getObject(index+1); // count(*)
 			if (maxSub == null) {
 				maxSub = 0;
 			}
-			result.add(new Pair<>(project, maxSub));
+			result.add(new Pair<>(project, maxSub.intValue()));
 		}
 		return result;
 	}
