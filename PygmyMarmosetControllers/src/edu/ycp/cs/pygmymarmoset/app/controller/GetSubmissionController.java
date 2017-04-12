9f2cafa644c8cd6e96107072f2724b71be6db25f
David Hovemeyer <david.hovemeyer@gmail.com>
2017-04-12 08:42:49 -0400

    clean up unused imports

--- PygmyMarmosetControllers/src/edu/ycp/cs/pygmymarmoset/app/controller/GetSubmissionController.java
+++ PygmyMarmosetControllers/src/edu/ycp/cs/pygmymarmoset/app/controller/GetSubmissionController.java
@@ -1,8 +1,6 @@
 package edu.ycp.cs.pygmymarmoset.app.controller;
 
-import edu.ycp.cs.pygmymarmoset.app.model.Project;
 import edu.ycp.cs.pygmymarmoset.app.model.Submission;
-import edu.ycp.cs.pygmymarmoset.app.model.User;
 import edu.ycp.cs.pygmymarmoset.model.persist.DatabaseProvider;
 import edu.ycp.cs.pygmymarmoset.model.persist.IDatabase;
 
