2ea17413b13241a0d8e64b6dade5b5bc5ca7e51d
David Hovemeyer <david.hovemeyer@gmail.com>
2017-05-09 11:50:04 -0400

    begin refactoring to support running from an uberjar

---
+++ PygmyMarmosetLauncher/src/edu/ycp/cs/pygmymarmoset/main/Launcher.java
@@ -0,0 +1,60 @@
+// Pygmy Marmoset - an assignment submission webapp for CS courses
+// Copyright (c) 2017, David H. Hovemeyer <david.hovemeyer@gmail.com>
+//
+// This is free software distributed under the terms of the
+// GNU Affero Public License v3 or later.  See LICENSE.txt for details.
+
+package edu.ycp.cs.pygmymarmoset.main;
+
+import java.io.File;
+import java.lang.management.ManagementFactory;
+
+import org.eclipse.jetty.jmx.MBeanContainer;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.Configuration;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+public class Launcher {
+	// This is adapted from the Embedded Jetty example from Jetty 9.4.x:
+	//	    https://www.eclipse.org/jetty/documentation/9.4.x/embedded-examples.html#embedded-webapp-jsp
+	public static Server launch(boolean fromEclipse, int port, String webappCodeBase) throws Exception {
+		Server server = new Server(port);
+
+		MBeanContainer mbContainer = new MBeanContainer(ManagementFactory.getPlatformMBeanServer());
+		server.addBean(mbContainer);
+
+		WebAppContext webapp = new WebAppContext();
+		webapp.setContextPath("/marmoset");
+		File warFile = new File(webappCodeBase); // FIXME
+		webapp.setWar(warFile.getAbsolutePath());
+
+		Configuration.ClassList classList = Configuration.ClassList.setServerDefault(server);
+		classList.addBefore(
+				"org.eclipse.jetty.webapp.JettyWebXmlConfiguration",
+				"org.eclipse.jetty.annotations.AnnotationConfiguration");
+		webapp.setAttribute(
+				"org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern",
+				".*/[^/]*servlet-api-[^/]*\\.jar$|.*/javax.servlet.jsp.jstl-.*\\.jar$|.*/[^/]*taglibs.*\\.jar$" );
+
+		// Don't allow directory listings
+		webapp.setInitParameter("org.eclipse.jetty.servlet.Default.dirAllowed", "false");
+
+		// Allow the welcome file to be a servlet
+		webapp.setInitParameter("org.eclipse.jetty.servlet.Default.welcomeServlets", "true");
+
+		if (fromEclipse) {
+			// Set up "extra" classpath directories/jarfiles from referenced
+			// Eclipse projects.  When launching from the uberjar, these
+			// aren't needed because all of the required classes are
+			// contained in the webapp's WEB-INF/classes directory.
+			webapp.setExtraClasspath("../PygmyMarmosetModelClasses/bin/,../PygmyMarmosetPersistence/bin/,../PygmyMarmosetControllers/bin/");
+		}
+		
+		server.setHandler(webapp);
+
+		server.start();
+		
+		return server;
+	}
+
+}
--- PygmyMarmosetLauncher/src/edu/ycp/cs/pygmymarmoset/main/Main.java
+++ PygmyMarmosetLauncher/src/edu/ycp/cs/pygmymarmoset/main/Main.java
@@ -6,58 +6,14 @@
 
 package edu.ycp.cs.pygmymarmoset.main;
 
-import java.io.File;
-import java.lang.management.ManagementFactory;
-
-import org.eclipse.jetty.jmx.MBeanContainer;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.webapp.Configuration;
-import org.eclipse.jetty.webapp.WebAppContext;
 
-// This is adapted from the Embedded Jetty example from Jetty 9.4.x:
-//    https://www.eclipse.org/jetty/documentation/9.4.x/embedded-examples.html#embedded-webapp-jsp
-
+// Run interactively from Eclipse.
 public class Main {
 	private static final int PORT = 8080;
 
 	public static void main(String[] args) throws Exception {
-		launch(true);
-	}
-	
-	public static void launch(boolean fromEclipse) throws Exception {
-		Server server = new Server(PORT);
-
-		MBeanContainer mbContainer = new MBeanContainer(ManagementFactory.getPlatformMBeanServer());
-		server.addBean(mbContainer);
-
-		WebAppContext webapp = new WebAppContext();
-		webapp.setContextPath("/marmoset");
-		File warFile = new File("../PygmyMarmosetWebapp/war"); // FIXME
-		webapp.setWar(warFile.getAbsolutePath());
-
-		Configuration.ClassList classList = Configuration.ClassList.setServerDefault(server);
-		classList.addBefore(
-				"org.eclipse.jetty.webapp.JettyWebXmlConfiguration",
-				"org.eclipse.jetty.annotations.AnnotationConfiguration");
-		webapp.setAttribute(
-				"org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern",
-				".*/[^/]*servlet-api-[^/]*\\.jar$|.*/javax.servlet.jsp.jstl-.*\\.jar$|.*/[^/]*taglibs.*\\.jar$" );
-
-		// Don't allow directory listings
-		webapp.setInitParameter("org.eclipse.jetty.servlet.Default.dirAllowed", "false");
-
-		// Allow the welcome file to be a servlet
-		webapp.setInitParameter("org.eclipse.jetty.servlet.Default.welcomeServlets", "true");
-
-		if (fromEclipse) {
-			// Set up "extra" classpath directories/jarfiles from referenced
-			// Eclipse projects.
-			webapp.setExtraClasspath("../PygmyMarmosetModelClasses/bin/,../PygmyMarmosetPersistence/bin/,../PygmyMarmosetControllers/bin/");
-		}
-		
-		server.setHandler(webapp);
-
-		server.start();
+		Server server = Launcher.launch(true, PORT, "../PygmyMarmosetWebapp/war");
 		server.dumpStdErr();
 		server.join();
 	}
