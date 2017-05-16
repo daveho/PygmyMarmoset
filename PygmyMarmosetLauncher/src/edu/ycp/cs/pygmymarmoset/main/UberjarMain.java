2bf3c90db7f7192aa12cde00129ac775c8d113f1
David Hovemeyer <david.hovemeyer@gmail.com>
2017-05-16 09:15:40 -0400

    added createdb command (to create database from command line)
    This uses NestedJarClassLoader from CloudCoder.  It also
    required packaging the classes depended on by the webapp
    as jarfiles rather than including them directly in
    war/WEB-INF/classes.

--- PygmyMarmosetLauncher/src/edu/ycp/cs/pygmymarmoset/main/UberjarMain.java
+++ PygmyMarmosetLauncher/src/edu/ycp/cs/pygmymarmoset/main/UberjarMain.java
@@ -1,9 +1,22 @@
 package edu.ycp.cs.pygmymarmoset.main;
 
+import java.util.Arrays;
+import java.util.List;
+
 // Launch from an uberjar.
 public class UberjarMain {
-	public static void main(String[] args) {
-		UberjarDaemonController controller = new UberjarDaemonController();
-		controller.exec(args);
+	public static void main(String[] args) throws Exception {
+		// Check to see if the first argument is an admin command.
+		String cmd = args.length > 0 ? args[0] : "";
+		if (cmd.equals("createdb")) {
+			NestedJarClassLoader.runMain(UberjarMain.class, "edu.ycp.cs.pygmymarmoset.model.persist.CreateDatabase", trimArgs(args));
+		} else {
+			UberjarDaemonController controller = new UberjarDaemonController();
+			controller.exec(args);
+		}
+	}
+	
+	private static List<String> trimArgs(String[] args) {
+		return Arrays.asList(args).subList(1, args.length);
 	}
 }
